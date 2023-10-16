42421970c021c2b76ff38c565f9350aded7a4cfd
kasun <kasunvimu8@gmail.com>
2023-10-16 23:00:03 +0200

    Finance logger resources

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var chartactor = "kasun";
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (inputs) {
+    console.log(inputs);
+});
---
+++ sandbox.ts
@@ -0,0 +1,7 @@
+const chartactor = "kasun";
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach((inputs) => {
+    console.log(inputs);
+});
